d753eda3e220db6df4ed86e28a979ee314af047f
Zaid <ttrmux@proton.me>
2026-03-04 22:11:04 +0300

    Remove nvim configuration permanently

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Redo shortcut
-vim.keymap.set("n", "<M-r>", "<C-r>", { noremap = true })
--- Open the netrw file explorer
-vim.keymap.set("n", "<leader>cd", ":Ex<CR>", { desc = "Open netrw file explorer" })
--- .config/nvim/lua/plugins/onedark.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "navarasu/onedark.nvim",
-  version = "v0.1.0",
-  priority = 1000,
-  opts = {
-    style = "darker",
-  },
-}
